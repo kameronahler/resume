b339e95c5b7523b64d3050103f2fa0006fc7b32d
Kameron <kamahler@gmail.com>
2021-03-25 13:07:19 -0600

    stage some starter files

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Entry } from './Entry'
+
+const App = () => {
+  return (
+    <>
+      <Entry />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Entry.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Entry = () => {
+  return <h1>Parcel + React</h1>
+}
+
+export default Entry
