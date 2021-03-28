537b447c865334ca4c344e524775608c8d0c4ce3
Kameron <kamahler@gmail.com>
2021-03-27 23:51:32 -0600

    add postcss nesting and import

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: [
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
