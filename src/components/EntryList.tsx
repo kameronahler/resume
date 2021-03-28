b682b2b538f0611679a7b22a43e4fd20aaee8e35
Kameron <kamahler@gmail.com>
2021-03-28 14:32:57 -0600

    style ul items

--- src/components/EntryList.tsx
+++ src/components/EntryList.tsx
@@ -4,9 +4,22 @@ import { PropsEntryList } from './Types'
 
 const EntryList = ({ listJSON }: PropsEntryList) => {
   return (
-    <ul>
+    <ul className='entry-list'>
       {listJSON.list.map((item, i) => {
-        return <li key={i}>{item}</li>
+        return (
+          <li className='entry-list__item' key={i}>
+            <svg
+              aria-hidden='true'
+              className='entry-list__item-bullet'
+              fill='none'
+              viewBox='0 0 4 4'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <circle cx='2' cy='2' r='1.5' />
+            </svg>
+            <span>{item}</span>
+          </li>
+        )
       })}
     </ul>
   )
