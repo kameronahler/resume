602b93f28cb79195bac05db28c2bbdb014275b4e
Kameron <kamahler@gmail.com>
2021-03-31 00:26:46 -0600

    prevent right-hand popovers from overflowing app

--- src/components/Popover/PopoverOpen.tsx
+++ src/components/Popover/PopoverOpen.tsx
@@ -1,20 +1,29 @@
 import React, { useState, forwardRef } from 'react'
 import PopoverOverlay from './PopoverOverlay'
 
-const TOGGLE_CLASS = 'popover__content--collapsed'
+const COLLAPSED_CLASS = 'popover__content--collapsed'
+const RIGHT_HAND_SIDE_CLASS = 'popover__content--right-hand-side'
 
 const PopoverOpen = forwardRef((props, ref) => {
   const [expanded, setExpanded] = useState(false)
 
-  const handleOpen = () => {
+  const getPositionClass = (e: object): string | null => {
+    const appCoords = document.getElementById('app').getBoundingClientRect()
+    const appMiddle = appCoords.width / 2 + appCoords.x
+    const buttonCoords = e.currentTarget.getBoundingClientRect()
+    return buttonCoords.x >= appMiddle ? RIGHT_HAND_SIDE_CLASS : null
+  }
+
+  const handleOpen = (e: object): void => {
     ref.current.setAttribute('aria-expanded', 'true')
-    ref.current.classList.remove(TOGGLE_CLASS)
+    ref.current.classList.remove(COLLAPSED_CLASS)
+    ref.current.classList.add(getPositionClass(e))
     setExpanded(true)
   }
 
-  const handleClose = () => {
+  const handleClose = (): void => {
     ref.current.setAttribute('aria-expanded', 'false')
-    ref.current.classList.add(TOGGLE_CLASS)
+    ref.current.classList.add(COLLAPSED_CLASS)
     setExpanded(false)
   }
 
@@ -25,11 +34,11 @@ const PopoverOpen = forwardRef((props, ref) => {
         className={`popover__button ${
           expanded ? 'popover__button--expanded' : ''
         }`}
-        onClick={() => {
+        onClick={e => {
           if (expanded) {
             handleClose()
           } else {
-            handleOpen()
+            handleOpen(e)
           }
         }}
       >
